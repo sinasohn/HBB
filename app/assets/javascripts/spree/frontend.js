7134e742beac200d6f58aaa2211ac1579c600b70
captproton <carl@wdwhub.net>
2018-07-03 20:14:25 -0700

    CHANGE: amazon-inspired search bar created

--- app/assets/javascripts/spree/frontend.js
+++
@@ -1,8 +0,0 @@
-//= require jquery2
-//= require jquery_ujs
-//= require popper
-//= require spree
-//= require spree/frontend/checkout
-//= require spree/frontend/product
-//= require spree/frontend/cart
-//= require spree/frontend/locale_selector
