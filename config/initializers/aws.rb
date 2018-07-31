e5db173f5f721882ae6bc6258cdeb9f9b9b183ca
captproton <carl@wdwhub.net>
2018-07-30 20:28:49 -0700

    CHANGE: attempt to address  in production

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -28,6 +28,3 @@ attachment_config = {
 attachment_config.each do |key, value|
    Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
 end unless Rails.env.test?
-
-# workaround for `NameError (uninitialized constant Aws::VERSION)`
-Aws::VERSION =  Gem.loaded_specs["aws-sdk"].version
